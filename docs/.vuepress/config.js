5df691ef7db1936059a878c49600851a5c426964
lucas <xieyan9@tal.com>
2022-07-03 16:52:33 +0800

    public

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,12 +1,23 @@
 module.exports = {
     title: 'Hello VuePress',
     description: 'Just playing around',
-    base: '/dev/',
+    base: '/',
+    dest: 'public',
     configureWebpack: {
         resolve: {
             alias: {
             '@alias': 'path/to/some/dir'
             }
+        }
+    },
+    markdown: {
+        // markdown-it-anchor 的选项
+        anchor: { permalink: false },
+        // markdown-it-toc 的选项
+        toc: { includeLevel: [1, 2] },
+        extendMarkdown: md => {
+            // 使用更多的 markdown-it 插件!
+            md.use(require('markdown-it-xxx'))
         }
     }
 }
