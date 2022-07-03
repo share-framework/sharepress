dc4d0b1a74991fd88217bff3be29c26f06374870
andotorg <andotorg@outlook.com>
2022-07-03 18:57:20 +0800

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,12 +2,5 @@ module.exports = {
     title: 'Hello VuePress',
     description: 'Just playing around',
     base: '/',
-    dest: 'public',
-    configureWebpack: {
-        resolve: {
-            alias: {
-            '@alias': 'path/to/some/dir'
-            }
-        }
-    }
+    dest: 'public'
 }
